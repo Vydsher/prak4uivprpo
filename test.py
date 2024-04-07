d2bc0e559777fbcb7aa580d87ccac548709c7f78
Vydsher <delov_sergeyy@rambler.ru>
2024-04-07 19:03:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def test_clean_text():
     assert clean_text("John Doe") == "[censored]"
 
     # Тестирование замены номеров телефонов
-    assert clean_text("Phone: +79775510001") == "[censored]"
+    assert clean_text("Phone: +78881231213") == "[censored]"
 
     # Тестирование замены геолокации
     assert clean_text("IP Address: 192.168.1.1") == "[censored]"
