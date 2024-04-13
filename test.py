62c1bd26c32398aec50a79cdaa3b81bcc79e9618
Vydsher <delov_sergeyy@rambler.ru>
2024-04-13 08:03:07 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,16 @@
+from main import clean_text
 import re
-import unittest
-from main import clean_text  # Замените your_script_file на имя вашего файла скрипта
+def test_clean_text():
+    # Тестирование замены имен и фамилий
+    assert clean_text("John Doe") == "[censored]"
 
-class TestCleanText(unittest.TestCase):
-    def test_names_censored(self):
-        text = "John Doe and Jane Smith"
-        expected_output = "[censored] and [censored]"
-        self.assertEqual(clean_text(text), expected_output)
+    # Тестирование замены номеров телефонов
+    assert clean_text("Его номер: +78881231213") == "Его номер: [censored]"
 
-    def test_phone_numbers_censored(self):
-        text = "Call me at +12345678901"
-        expected_output = "Call me at [censored]"
-        self.assertEqual(clean_text(text), expected_output)
+    # Тестирование замены геолокации
+    assert clean_text("IP Address: 192.168.1.1") == "IP Address: [censored]"
 
-    def test_geo_data_censored(self):
-        text = "My IP address is 192.168.0.1"
-        expected_output = "My IP address is [censored]"
-        self.assertEqual(clean_text(text), expected_output)
+    print("All tests passed successfully!")
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    test_clean_text()
